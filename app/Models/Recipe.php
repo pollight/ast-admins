e22790aeaf843d1532b7042711d1128711728ae0
Pavel Zholnerovich <pavel.zholnerovich@yandex.ru>
2019-09-23 13:05:28 +0300

    model Recipe and migration

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -6,5 +6,133 @@ use Illuminate\Database\Eloquent\Model;
 
 class Recipe extends Model
 {
-    //
+    protected $fillable = [
+        'Name',
+        'userId',
+        'gost',
+        'used',
+
+        'knotsIntegrated_plast_enable',
+
+        'knotsIntegrated_plast_size1',
+        'knotsIntegrated_plast_size2',
+        'knotsIntegrated_plast_size3',
+        'knotsIntegrated_plast_size4',
+        'knotsIntegrated_plast_size5',
+
+        'knotsIntegrated_plast_quantity1',
+        'knotsIntegrated_plast_quantity2',
+        'knotsIntegrated_plast_quantity3',
+        'knotsIntegrated_plast_quantity4',
+        'knotsIntegrated_plast_quantity5',
+
+        'knotsIntegrated_kormkaDo40_enable',
+
+        'knotsIntegrated_kormkaDo40_size1',
+        'knotsIntegrated_kormkaDo40_size2',
+        'knotsIntegrated_kormkaDo40_size3',
+        'knotsIntegrated_kormkaDo40_size4',
+        'knotsIntegrated_kormkaDo40_size5',
+
+        'knotsIntegrated_kormkaDo40_quantity1',
+        'knotsIntegrated_kormkaDo40_quantity2',
+        'knotsIntegrated_kormkaDo40_quantity3',
+        'knotsIntegrated_kormkaDo40_quantity4',
+        'knotsIntegrated_kormkaDo40_quantity5',
+
+        'knotsIntegrated_kormkaPosle40_enable',
+
+        'knotsIntegrated_kormkaPosle40_size1',
+        'knotsIntegrated_kormkaPosle40_size2',
+        'knotsIntegrated_kormkaPosle40_size3',
+        'knotsIntegrated_kormkaPosle40_size4',
+        'knotsIntegrated_kormkaPosle40_size5',
+
+        'knotsIntegrated_kormkaPosle40_quantity1',
+        'knotsIntegrated_kormkaPosle40_quantity2',
+        'knotsIntegrated_kormkaPosle40_quantity3',
+        'knotsIntegrated_kormkaPosle40_quantity4',
+        'knotsIntegrated_kormkaPosle40_quantity5',
+
+        'knotsNotIntagrated__plast_enable',
+
+        'knotsNotIntagrated__plast_size1',
+        'knotsNotIntagrated__plast_size2',
+        'knotsNotIntagrated__plast_size3',
+        'knotsNotIntagrated__plast_size4',
+        'knotsNotIntagrated__plast_size5',
+
+        'knotsNotIntagrated__plast_quantity1',
+        'knotsNotIntagrated__plast_quantity2',
+        'knotsNotIntagrated__plast_quantity3',
+        'knotsNotIntagrated__plast_quantity4',
+        'knotsNotIntagrated__plast_quantity5',
+
+        'knotsNotIntagrated_kormkaDo40_enable',
+
+        'knotsNotIntagrated_kormkaDo40_size1',
+        'knotsNotIntagrated_kormkaDo40_size2',
+        'knotsNotIntagrated_kormkaDo40_size3',
+        'knotsNotIntagrated_kormkaDo40_size4',
+        'knotsNotIntagrated_kormkaDo40_size5',
+
+        'knotsNotIntagrated_kormkaDo40_quantity1',
+        'knotsNotIntagrated_kormkaDo40_quantity2',
+        'knotsNotIntagrated_kormkaDo40_quantity3',
+        'knotsNotIntagrated_kormkaDo40_quantity4',
+        'knotsNotIntagrated_kormkaDo40_quantity5',
+
+        'knotsNotIntagrated_kormkaPosle40_enable',
+
+        'knotsNotIntagrated_kormkaPosle40_size1',
+        'knotsNotIntagrated_kormkaPosle40_size2',
+        'knotsNotIntagrated_kormkaPosle40_size3',
+        'knotsNotIntagrated_kormkaPosle40_size4',
+        'knotsNotIntagrated_kormkaPosle40_size5',
+
+        'knotsNotIntagrated_kormkaPosle40_quantity1',
+        'knotsNotIntagrated_kormkaPosle40_quantity2',
+        'knotsNotIntagrated_kormkaPosle40_quantity3',
+        'knotsNotIntagrated_kormkaPosle40_quantity4',
+        'knotsNotIntagrated_kormkaPosle40_quantity5',
+
+        'knotsWithRot_enable',
+
+        'knotsWithRot_size1',
+        'knotsWithRot_size2',
+        'knotsWithRot_size3',
+        'knotsWithRot_size4',
+        'knotsWithRot_size5',
+
+        'knotsWithRot_quantity1',
+        'knotsWithRot_quantity2',
+        'knotsWithRot_quantity3',
+        'knotsWithRot_quantity4',
+        'knotsWithRot_quantity5',
+
+        'sineva_enable',
+
+        'sineva_size1',
+        'sineva_size2',
+        'sineva_size3',
+        'sineva_size4',
+        'sineva_size5',
+
+        'obzolBlunt_enable',
+
+        'obzolBlunt_size1',
+        'obzolBlunt_size2',
+        'obzolBlunt_size3',
+        'obzolBlunt_size4',
+        'obzolBlunt_size5',
+
+        'obzolSharp_enable',
+
+        'obzolSharp_size1',
+        'obzolSharp_size2',
+        'obzolSharp_size3',
+        'obzolSharp_size4',
+        'obzolSharp_size5',
+        
+    ];
 }
--- database/migrations/2019_09_14_184138_create_recipes_table.php
+++ database/migrations/2019_09_14_184138_create_recipes_table.php
@@ -14,7 +14,135 @@ class CreateRecipesTable extends Migration
     public function up()
     {
         Schema::create('recipes', function (Blueprint $table) {
+
             $table->bigIncrements('id');
+
+            $table->string('Name')->unique();
+            $table->bigInteger('userId')->index();
+            $table->string('gost')->index()->nullable();
+            $table->boolean('used')->index();
+
+            $table->boolean('knotsIntegrated_plast_enable')->nullable();
+            $table->float('knotsIntegrated_plast_size1', 8, 2)->nullable();
+            $table->float('knotsIntegrated_plast_size2', 8, 2)->nullable();
+            $table->float('knotsIntegrated_plast_size3', 8, 2)->nullable();
+            $table->float('knotsIntegrated_plast_size4', 8, 2)->nullable();
+            $table->float('knotsIntegrated_plast_size5', 8, 2)->nullable();
+
+            $table->bigInteger('knotsIntegrated_plast_quantity1')->nullable();
+            $table->bigInteger('knotsIntegrated_plast_quantity2')->nullable();
+            $table->bigInteger('knotsIntegrated_plast_quantity3')->nullable();
+            $table->bigInteger('knotsIntegrated_plast_quantity4')->nullable();
+            $table->bigInteger('knotsIntegrated_plast_quantity5')->nullable();
+
+            $table->boolean('knotsIntegrated_kormkaDo40_enable');
+
+            $table->float('knotsIntegrated_kormkaDo40_size1', 8, 2)->nullable();
+            $table->float('knotsIntegrated_kormkaDo40_size2', 8, 2)->nullable();
+            $table->float('knotsIntegrated_kormkaDo40_size3', 8, 2)->nullable();
+            $table->float('knotsIntegrated_kormkaDo40_size4', 8, 2)->nullable();
+            $table->float('knotsIntegrated_kormkaDo40_size5', 8, 2)->nullable();
+
+            $table->bigInteger('knotsIntegrated_kormkaDo40_quantity1')->nullable();
+            $table->bigInteger('knotsIntegrated_kormkaDo40_quantity2')->nullable();
+            $table->bigInteger('knotsIntegrated_kormkaDo40_quantity3')->nullable();
+            $table->bigInteger('knotsIntegrated_kormkaDo40_quantity4')->nullable();
+            $table->bigInteger('knotsIntegrated_kormkaDo40_quantity5')->nullable();
+
+            $table->boolean('knotsIntegrated_kormkaPosle40_enable');
+
+            $table->float('knotsIntegrated_kormkaPosle40_size1', 8, 2)->nullable();
+            $table->float('knotsIntegrated_kormkaPosle40_size2', 8, 2)->nullable();
+            $table->float('knotsIntegrated_kormkaPosle40_size3', 8, 2)->nullable();
+            $table->float('knotsIntegrated_kormkaPosle40_size4', 8, 2)->nullable();
+            $table->float('knotsIntegrated_kormkaPosle40_size5', 8, 2)->nullable();
+
+            $table->bigInteger('knotsIntegrated_kormkaPosle40_quantity1')->nullable();
+            $table->bigInteger('knotsIntegrated_kormkaPosle40_quantity2')->nullable();
+            $table->bigInteger('knotsIntegrated_kormkaPosle40_quantity3')->nullable();
+            $table->bigInteger('knotsIntegrated_kormkaPosle40_quantity4')->nullable();
+            $table->bigInteger('knotsIntegrated_kormkaPosle40_quantity5')->nullable();
+
+            $table->boolean('knotsNotIntagrated__plast_enable');
+
+            $table->float('knotsNotIntagrated__plast_size1', 8, 2)->nullable();
+            $table->float('knotsNotIntagrated__plast_size2', 8, 2)->nullable();
+            $table->float('knotsNotIntagrated__plast_size3', 8, 2)->nullable();
+            $table->float('knotsNotIntagrated__plast_size4', 8, 2)->nullable();
+            $table->float('knotsNotIntagrated__plast_size5', 8, 2)->nullable();
+
+            $table->bigInteger('knotsNotIntagrated__plast_quantity1')->nullable();
+            $table->bigInteger('knotsNotIntagrated__plast_quantity2')->nullable();
+            $table->bigInteger('knotsNotIntagrated__plast_quantity3')->nullable();
+            $table->bigInteger('knotsNotIntagrated__plast_quantity4')->nullable();
+            $table->bigInteger('knotsNotIntagrated__plast_quantity5')->nullable();
+
+            $table->boolean('knotsNotIntagrated_kormkaDo40_enable');
+
+            $table->float('knotsNotIntagrated_kormkaDo40_size1', 8, 2)->nullable();
+            $table->float('knotsNotIntagrated_kormkaDo40_size2', 8, 2)->nullable();
+            $table->float('knotsNotIntagrated_kormkaDo40_size3', 8, 2)->nullable();
+            $table->float('knotsNotIntagrated_kormkaDo40_size4', 8, 2)->nullable();
+            $table->float('knotsNotIntagrated_kormkaDo40_size5', 8, 2)->nullable();
+
+            $table->bigInteger('knotsNotIntagrated_kormkaDo40_quantity1')->nullable();
+            $table->bigInteger('knotsNotIntagrated_kormkaDo40_quantity2')->nullable();
+            $table->bigInteger('knotsNotIntagrated_kormkaDo40_quantity3')->nullable();
+            $table->bigInteger('knotsNotIntagrated_kormkaDo40_quantity4')->nullable();
+            $table->bigInteger('knotsNotIntagrated_kormkaDo40_quantity5')->nullable();
+
+            $table->boolean('knotsNotIntagrated_kormkaPosle40_enable');
+
+            $table->float('knotsNotIntagrated_kormkaPosle40_size1', 8, 2)->nullable();
+            $table->float('knotsNotIntagrated_kormkaPosle40_size2', 8, 2)->nullable();
+            $table->float('knotsNotIntagrated_kormkaPosle40_size3', 8, 2)->nullable();
+            $table->float('knotsNotIntagrated_kormkaPosle40_size4', 8, 2)->nullable();
+            $table->float('knotsNotIntagrated_kormkaPosle40_size5', 8, 2)->nullable();
+
+            $table->bigInteger('knotsNotIntagrated_kormkaPosle40_quantity1')->nullable();
+            $table->bigInteger('knotsNotIntagrated_kormkaPosle40_quantity2')->nullable();
+            $table->bigInteger('knotsNotIntagrated_kormkaPosle40_quantity3')->nullable();
+            $table->bigInteger('knotsNotIntagrated_kormkaPosle40_quantity4')->nullable();
+            $table->bigInteger('knotsNotIntagrated_kormkaPosle40_quantity5')->nullable();
+
+            $table->boolean('knotsWithRot_enable');
+
+            $table->float('knotsWithRot_size1', 8, 2)->nullable();
+            $table->float('knotsWithRot_size2', 8, 2)->nullable();
+            $table->float('knotsWithRot_size3', 8, 2)->nullable();
+            $table->float('knotsWithRot_size4', 8, 2)->nullable();
+            $table->float('knotsWithRot_size5', 8, 2)->nullable();
+
+            $table->bigInteger('knotsWithRot_quantity1')->nullable();
+            $table->bigInteger('knotsWithRot_quantity2')->nullable();
+            $table->bigInteger('knotsWithRot_quantity3')->nullable();
+            $table->bigInteger('knotsWithRot_quantity4')->nullable();
+            $table->bigInteger('knotsWithRot_quantity5')->nullable();
+
+            $table->boolean('sineva_enable');
+
+            $table->bigInteger('sineva_size1')->nullable();
+            $table->bigInteger('sineva_size2')->nullable();
+            $table->bigInteger('sineva_size3')->nullable();
+            $table->bigInteger('sineva_size4')->nullable();
+            $table->bigInteger('sineva_size5')->nullable();
+
+            $table->boolean('obzolBlunt_enable');
+
+            $table->float('obzolBlunt_size1', 8, 2)->nullable();
+            $table->float('obzolBlunt_size2', 8, 2)->nullable();
+            $table->float('obzolBlunt_size3', 8, 2)->nullable();
+            $table->float('obzolBlunt_size4', 8, 2)->nullable();
+            $table->float('obzolBlunt_size5', 8, 2)->nullable();
+
+            $table->boolean('obzolSharp_enable');
+
+            $table->float('obzolSharp_size1', 8, 2)->nullable();
+            $table->float('obzolSharp_size2', 8, 2)->nullable();
+            $table->float('obzolSharp_size3', 8, 2)->nullable();
+            $table->float('obzolSharp_size4', 8, 2)->nullable();
+            $table->float('obzolSharp_size5', 8, 2)->nullable();
+
             $table->timestamps();
         });
     }
